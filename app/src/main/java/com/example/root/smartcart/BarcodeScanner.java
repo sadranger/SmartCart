048b9c0af1061628daa7e6ebcd7bc16dedbf5dab
Řãňjîţh Şhäğåńťï <devil@localhost.localdomain>
2018-03-03 07:20:28 +0530

    first

---
+++ app/src/main/java/com/example/root/smartcart/BarcodeScanner.java
@@ -0,0 +1,187 @@
+package com.example.root.smartcart;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+import static android.hardware.Camera.CameraInfo.CAMERA_FACING_BACK;
+
+/**
+ * Created by root on 2/3/18.
+ */
+
+public class BarcodeScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+    private static int camId = CAMERA_FACING_BACK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        int currentApiVersion = Build.VERSION.SDK_INT;
+
+        if(currentApiVersion >=  Build.VERSION_CODES.M)
+        {
+            if(checkPermission())
+            {
+                //Toast.makeText(getApplicationContext(), "Permission already granted!", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                requestPermission();
+            }
+        }
+    }
+
+    private boolean checkPermission()
+    {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission()
+    {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if(scannerView == null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        scannerView.stopCamera();
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted){
+                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA},
+                                                            REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(BarcodeScanner.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        String src1,src2,res;
+        res=result.getText().toString();
+        src1="123456";
+        src2="654321";
+
+        if(src1.equals(res)) {
+
+            Intent intent = new Intent(BarcodeScanner.this,Shopping.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("name","iPhone X");
+            bundle.putString("price","94,490");
+            bundle.putString("offers","10% discount on Axis Credit cards");
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+
+        }
+        else if(src2.equals(res)){
+
+            Intent intent = new Intent(BarcodeScanner.this,Shopping.class);
+            Bundle bundle2 = new Bundle();
+            bundle2.putString("name","MacBook Pro");
+            bundle2.putString("price","1,90,900");
+            bundle2.putString("offers","Student offer of 10%");
+            intent.putExtras(bundle2);
+            startActivity(intent);
+
+        }
+        else{
+            Intent intent = new Intent(BarcodeScanner.this,Shopping.class);
+            startActivity(intent);
+            Toast.makeText(this, "Item out of stock. Inconvenience caused is deeply regretted!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Failed to get details!!", Toast.LENGTH_SHORT).show();
+        }
+
+        /*final String myResult = result.getText();
+        Log.d("QRCodeScanner", result.getText());
+        Log.d("QRCodeScanner", result.getBarcodeFormat().toString());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                scannerView.resumeCameraPreview(QrScanner.this);
+            }
+        });
+        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(myResult));
+                startActivity(browserIntent);
+            }
+        });
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();*/
+    }
+}
+
---
+++ app/src/main/java/com/example/root/smartcart/Shopping.java
@@ -0,0 +1,169 @@
+package com.example.root.smartcart;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Shopping extends AppCompatActivity {
+
+    Button throwin,throwout,bt,done;
+    TextView nameT,priceT,offersT;
+    int REQUEST_ENABLE_BT=1;
+    BluetoothAdapter bAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    String plist[];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+
+        throwin = (Button) findViewById(R.id.throwin);
+        throwout = (Button) findViewById(R.id.throwout);
+        done = (Button) findViewById(R.id.done);
+        bt = (Button) findViewById(R.id.bluetooth);
+        nameT = (TextView) findViewById(R.id.nameres);
+        priceT = (TextView)  findViewById(R.id.priceres);
+        offersT = (TextView) findViewById(R.id.offersres);
+
+        try
+        {
+            Bundle bundle = getIntent().getExtras();
+            String name = bundle.getString("name");
+            String price = bundle.getString("price");
+            String offers = bundle.getString("offers");
+            nameT.setText(name);
+            priceT.setText(price);
+            offersT.setText(offers);
+        }
+        catch(Exception e ){
+            Log.e("error is: ",e.getMessage());
+        }
+        try {
+            Bundle bundle2 = getIntent().getExtras();
+            String name = bundle2.getString("name");
+            String price = bundle2.getString("price");
+            String offers = bundle2.getString("offers");
+        }
+        catch (Exception e){
+            Toast.makeText(this, "Failed to get details!!", Toast.LENGTH_SHORT).show();
+            Log.e("Error is:",e.getMessage());
+        }
+
+
+        throwin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shopping.this, BarcodeScanner.class);
+                startActivity(intent);
+            }
+        });
+        throwout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shopping.this, BarcodeScanner.class);
+                startActivity(intent);
+            }
+        });
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                bAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                if(bAdapter==null){
+
+                    Toast.makeText(Shopping.this, "Bluetooth is not available on this device!!", Toast.LENGTH_SHORT).show();
+
+
+                }
+                else{
+
+                if(!bAdapter.isEnabled()){
+
+                    Intent startBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(startBluetooth,REQUEST_ENABLE_BT);
+
+                }
+
+                }
+
+            }
+        });
+
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Shopping.this);
+                builder.setTitle("Are you sure to end shopping?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent (Shopping.this,Bill.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Shopping.this,Shopping.class);
+                        startActivity(intent);
+                    }
+                });
+                AlertDialog alert= builder.create();
+                alert.show();
+            }
+        });
+
+    }
+
+
+
+
+    public void onActivityResult(int requestcode,int resultcode,Intent data){
+
+        if(requestcode==REQUEST_ENABLE_BT){
+
+            if(resultcode==RESULT_OK){
+
+                Toast.makeText(this, "Bluetooth successfully turned ON!", Toast.LENGTH_SHORT).show();
+                pairedDevices = bAdapter.getBondedDevices();
+                int count = pairedDevices.size();
+                plist = new String[count];
+                int i=0;
+                for(BluetoothDevice device : pairedDevices){
+
+                    plist[i]=device.getName().toString();
+                    i++;
+
+                }
+                Bundle bn = new Bundle();
+                bn.putStringArray("paired",plist);
+                Intent pass = new Intent("paired_dev");
+                pass.putExtras(bn);
+                startActivity(pass);
+
+            }
+        }
+
+    }
+}
+
